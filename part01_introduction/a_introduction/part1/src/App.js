6fd42da2b70dca7833a61dd6e8feb7f60cfe32ae
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-09-13 19:59:40 +0200

    added part1-example03

--- part01_introduction/a_introduction/part1/src/App.js
+++ part01_introduction/a_introduction/part1/src/App.js
@@ -7,12 +7,29 @@ const App = () => (
   </div>
 )*/
 
-/*example02 : console*/
+/*example02 : console
 const App = () => {
   console.log('Hello from component')
   return (
     <div>
       <p>Hello world</p>
+    </div>
+  )
+}*/
+
+/*exmaple03: dynamic content render*/
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
     </div>
   )
 }
