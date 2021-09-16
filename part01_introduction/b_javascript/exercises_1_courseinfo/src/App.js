c7a22f00a75847172de34e731f79015f024021ad
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-09-16 19:38:33 +0200

    added 1_courseinfo_step4

--- part01_introduction/b_javascript/exercises_1_courseinfo/src/App.js
+++ part01_introduction/b_javascript/exercises_1_courseinfo/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
- /* course information: step 3 */
-const App = () => {
+///// 1_course information: step 3 
+/*const App = () => {
   const course = 'Half Stack application development'
   const part1 = {
     name: 'Fundamentals of React',
@@ -23,6 +23,61 @@ const App = () => {
       <p>{part2.name}: {part2.exercises}</p>
       <p>{part3.name}: {part3.exercises}</p>
       <p>Total: {part1.exercises + part2.exercises + part3.exercises}</p>
+    </div>
+  )
+}*/
+
+///// 1_course information: step 4 
+
+const Header = (props) => {  
+  console.log(props)
+  return (    
+    <div>      
+      <h1>{props.course}</h1>    
+    </div>  
+  )
+}
+
+const Content = (props) => {  
+  console.log(props)
+  return (    
+    <div>      
+      <h1>{props.parts}</h1>    
+    </div>  
+  )
+}
+
+const Total = (props) => {  
+  console.log(props)
+  return (    
+    <div>      
+      <h1>{props.parts}</h1>    
+    </div>  
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    part1: {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    part2: {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    part3: {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
