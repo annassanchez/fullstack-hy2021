0f814cc5727e5f2a91c8f73b4b45c310b664ac07
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-09-16 19:11:50 +0200

    replace unuseful data for the app

--- part01_introduction/part1_exercises/1_courseinfo/src/App.js
+++ part01_introduction/part1_exercises/1_courseinfo/src/App.js
@@ -1,4 +1,37 @@
 import React from 'react'
+
+/*step 0
+import React from 'react'
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+export default App*/
+
+
 //* step1
 /*
 const Header = (props) => {  
@@ -46,7 +79,8 @@ const App = () => {
 }*/
 
 
-const Header = (props) => {  
+const Header = (props) => { 
+  console.log(props) 
   return (    
     <div>      
       <h1>{props.course}</h1>    
@@ -55,6 +89,7 @@ const Header = (props) => {
 }
 
 const Part = (props) => {  
+  console.log(props)
   return (    
     <div>      
       <p>{props.part}: {props.exercises}</p>
@@ -63,6 +98,7 @@ const Part = (props) => {
 }
 
 const Content = (props) => {  
+  console.log(props)
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -79,6 +115,7 @@ const Content = (props) => {
 }
 
 const Total = (props) => {  
+  console.log(props)
   return (    
     <div>      
       <p>Number of exercises: {props.exercises}</p>    
